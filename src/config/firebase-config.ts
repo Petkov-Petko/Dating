105375e704fb0fab75113f843720af80e435079f
Petko Petkov <petkozlatilov1234@gmail.com>
2024-08-19 12:52:54 +0300

    Add Firebase configuration files
    This commit adds the Firebase configuration file to the project. It initializes the Firebase app and exports the authentication and database instances. The storage functionality is currently commented out.

---
+++ src/config/firebase-config.ts
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import { getAuth } from "firebase/auth";
+// import { getStorage } from "firebase/storage";
+// import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.FIREBASE_API_KEY,
+  authDomain: "datingapp-8e4ad.firebaseapp.com",
+  projectId: "datingapp-8e4ad",
+  storageBucket: "datingapp-8e4ad.appspot.com",
+  messagingSenderId: "236486004044", 
+  appId: "1:236486004044:web:6214713ecaf82db10f53e9",
+  measurementId: "G-PF8FLKKR21"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const database = getDatabase(app);
+// export const storage = getStorage(app);
