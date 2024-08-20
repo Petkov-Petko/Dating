bd0116dc14ff5c8de1b83881dfaeafe8e8b4d653
Petko Petkov <petkozlatilov1234@gmail.com>
2024-08-20 15:25:48 +0300

    Update App.tsx with React Router routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import './App.css'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PublicHome from "./pages/PublicHome/PublicHome";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 
 function App() {
-
   return (
-    <>
- 
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PublicHome />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
