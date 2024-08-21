0ec25f6703a7308f9b61341c01000fce3a7bb9a3
Petko Petkov <petkozlatilov1234@gmail.com>
2024-08-21 11:49:38 +0300

    Update PublicHome page with login and registration components

--- src/pages/PublicHome/PublicHome.tsx
+++ src/pages/PublicHome/PublicHome.tsx
@@ -1,12 +1,38 @@
-import PublicNavBar from "../../components/PublicNavbar/PublicNavBar"
-import "./PublicHome.scss"
+import Login from "../Login/Login";
+import Register from "../Register/Register";
+import "./PublicHome.scss";
+import { useState } from "react";
 
 const PublicHome = () => {
-  return (
-    <div>
-      <PublicNavBar />
-    </div>
-  )
-}
+  const [active, setActive] = useState("login");
 
-export default PublicHome
+  const handleActive = () => {
+    if (active === "login") {
+      setActive("register");
+    } else {
+      setActive("login");
+    }
+  };
+
+  return (
+    <div className="publicHome">
+      <div>
+        <img src="https://via.placeholder.com/550" alt="" />
+      </div>
+      <div>
+        {active === "login" ? (<Login />) : (<Register />)}
+        {active === "login" ? (
+          <p className="sign-up-label">
+            Don't have an account?<span onClick={handleActive} className="sign-up-link">Sign up</span>
+          </p>
+        ) : (
+          <p className="sign-up-label">
+            Already have an account?<span onClick={handleActive}  className="sign-up-link">Log in</span>
+          </p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default PublicHome;
