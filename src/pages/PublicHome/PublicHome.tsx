8616de93a98904be12dfe81e4738d9b93da67105
Petko Petkov <petkozlatilov1234@gmail.com>
2024-08-21 10:46:58 +0300

    Add PublicHome page component

---
+++ src/pages/PublicHome/PublicHome.tsx
@@ -0,0 +1,12 @@
+import PublicNavBar from "../../components/PublicNavbar/PublicNavBar"
+import "./PublicHome.scss"
+
+const PublicHome = () => {
+  return (
+    <div>
+      <PublicNavBar />
+    </div>
+  )
+}
+
+export default PublicHome
