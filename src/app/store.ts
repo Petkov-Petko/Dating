f434c4cda6a776ad04f0fb5b70e9e1d724b6be63
Petko Petkov <petkozlatilov1234@gmail.com>
2024-08-23 12:17:06 +0300

    Added RootState and AppDispatch types

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-
 import { rootReducer } from "./reducers";
 
 export const store = configureStore({
     reducer: {data : rootReducer},
 });
+
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
