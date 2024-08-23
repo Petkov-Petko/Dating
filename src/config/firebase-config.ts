f108e9943c226487a9f0313d962a5840d727b1e1
Petko Petkov <petkozlatilov1234@gmail.com>
2024-08-23 12:18:22 +0300

    Update firebase-config.ts to use VITE_API_KEY and

--- src/config/firebase-config.ts
+++ src/config/firebase-config.ts
@@ -4,12 +4,14 @@ import { getAuth } from "firebase/auth";
 // import { getStorage } from "firebase/storage";
 // import { getAnalytics } from "firebase/analytics";
 
+
 const firebaseConfig = {
-  apiKey: import.meta.env.FIREBASE_API_KEY,
+  apiKey: import.meta.env.VITE_API_KEY,
   authDomain: "datingapp-8e4ad.firebaseapp.com",
+  databaseURL: "https://datingapp-8e4ad-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "datingapp-8e4ad",
   storageBucket: "datingapp-8e4ad.appspot.com",
-  messagingSenderId: "236486004044", 
+  messagingSenderId: "236486004044",
   appId: "1:236486004044:web:6214713ecaf82db10f53e9",
   measurementId: "G-PF8FLKKR21"
 };
