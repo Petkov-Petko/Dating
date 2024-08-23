821837c729e7d230633e60ff08cbb5f021a697ea
Petko Petkov <petkozlatilov1234@gmail.com>
2024-08-23 12:16:09 +0300

    Update App component to use Redux store for user authentication and loading state

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,58 @@
 import "./App.css";
+import { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { getAuth } from "firebase/auth";
+import { loginUser, setLoading } from "./features/userSlice";
+import { AppDispatch, RootState } from "./app/store";
 import PublicHome from "./pages/PublicHome/PublicHome";
-import Login from "./pages/Login/Login";
-import Register from "./pages/Register/Register";
+import SetUpAccount from "./pages/SetUpAccount/SetUpAccount";
+import NavBar from "./components/NavBar/NavBar";
+import Home from "./pages/Home/Home";
 
 function App() {
+  const user = useSelector((state: RootState) => state.data.user.user);
+  const verified = useSelector((state: RootState) => state.data.user.verified);
+  const isLoading = useSelector(
+    (state: RootState) => state.data.user.isLoading
+  );
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    getAuth().onAuthStateChanged((user) => {
+      if (user) {
+        dispatch(
+          loginUser({
+            uid: user.uid,
+            username: user.displayName,
+            email: user.email,
+          })
+        );
+        dispatch(setLoading(false));
+      } else {
+        dispatch(setLoading(false));
+        console.log("No user is signed in");
+      }
+    });
+  }, []);
+
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<PublicHome />} />
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
-      </Routes>
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <>
+          {user && <NavBar />}
+          <Routes>
+            <Route
+              path="/"
+              element={
+                !user ? <PublicHome /> : !verified ? <SetUpAccount /> : <Home />
+              }
+            />
+          </Routes>
+        </>
+      )}
     </BrowserRouter>
   );
 }
