738b93b6c5a763bc8db9a38d14d78cdd4cd049b7
Petko Petkov <petkozlatilov1234@gmail.com>
2024-08-23 12:16:29 +0300

    Update rootReducer to use userSlice reducer in combineReducers

--- src/app/reducers.ts
+++ src/app/reducers.ts
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
+import { userSlice } from "../features/userSlice";
 
 export const rootReducer = combineReducers({
-  user: "slice",
+  user: userSlice.reducer,
 });
