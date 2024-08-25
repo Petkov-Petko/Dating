5fded0d28857e520280f49edd2e5525e3af617e2
Petko Petkov <petkozlatilov1234@gmail.com>
2024-08-25 11:58:05 +0300

    =Allow users to upload profile photo

--- src/components/SetUpPhotos/SetUpPhotos.tsx
+++ src/components/SetUpPhotos/SetUpPhotos.tsx
@@ -10,7 +10,9 @@ const SetUpPhotos = () => {
   const dispatch = useDispatch<AppDispatch>();
   const userId = useSelector((state: RootState) => state.data.user.user?.uid);
   const fileInput = useRef<HTMLInputElement>(null);
+  const profilePhotoInput = useRef<HTMLInputElement>(null);
   const [photos, setPhotos] = useState<File[]>([]);
+  const [profilePhoto, setProfilePhoto] = useState<File | null>(null);
 
   const finish = async () => {
     await verifyUser(userId);
@@ -22,19 +24,40 @@ const SetUpPhotos = () => {
     if (files) {
       const newPhotos: File[] = Array.from(files);
       setPhotos((prevPhotos) => [...prevPhotos, ...newPhotos]);
-    }    
+    }
+  };
+
+  const handleSetProfilePhoto = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (files) {
+      setProfilePhoto(files[0]);
+    }
+    
   };
 
   const handleInputClick = () => {
     fileInput.current?.click();
   };
-  console.log(photos);
+
+  const handleProfilePhotoClick = () => {
+    profilePhotoInput.current?.click();
+  };
+
+  const removePhoto = (index: number) => {
+    setPhotos(photos.filter((_, i) => i !== index));
+  };
 
   return (
     <div className="setUp_photos">
       <h1>Upload Your Photos</h1>
       <div>
-        <img className="set_up_profile_photo" src={assets.userProfile} alt="" />
+      <input
+          ref={profilePhotoInput}
+          style={{ display: "none" }}
+          type="file"
+          onChange={handleSetProfilePhoto}
+        ></input>
+        <img onClick={handleProfilePhotoClick} className="set_up_profile_photo" src={profilePhoto? URL.createObjectURL(profilePhoto) : assets.userProfile} alt="" />
       </div>
       <div>
         <input
@@ -48,8 +71,18 @@ const SetUpPhotos = () => {
           Add Photos
         </p>
         <div className="set_up_all_photos">
-          {photos.map((photo)=> {
-            return <img src={URL.createObjectURL(photo)} alt="" />;
+          {photos.map((photo, index) => {
+            return (
+              <div key={index} className="photo_container">
+                <img src={URL.createObjectURL(photo)} alt="" />
+                <span
+                  className="remove_photo"
+                  onClick={() => removePhoto(index)}
+                >
+                  X
+                </span>
+              </div>
+            );
           })}
         </div>
       </div>
