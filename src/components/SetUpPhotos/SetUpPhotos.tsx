21df86640f52a563af1d804cbb736a072dc634a5
Petko Petkov <petkozlatilov1234@gmail.com>
2024-08-25 11:47:03 +0300

    SetUpPhotos component to allow users to upload photos during account setup

--- src/components/SetUpPhotos/SetUpPhotos.tsx
+++ src/components/SetUpPhotos/SetUpPhotos.tsx
@@ -1,19 +1,34 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { assets } from "../../assets/assets";
 import "./SetUpPhotos.scss";
 import { AppDispatch, RootState } from "../../app/store";
-import {  setVerified } from "../../features/userSlice";
-import { useSelector } from "react-redux";
+import { setVerified } from "../../features/userSlice";
 import { verifyUser } from "../../service/db-service";
+import { useRef, useState } from "react";
 
 const SetUpPhotos = () => {
   const dispatch = useDispatch<AppDispatch>();
   const userId = useSelector((state: RootState) => state.data.user.user?.uid);
+  const fileInput = useRef<HTMLInputElement>(null);
+  const [photos, setPhotos] = useState<File[]>([]);
 
   const finish = async () => {
     await verifyUser(userId);
     dispatch(setVerified(true));
   };
+
+  const addPhotos = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (files) {
+      const newPhotos: File[] = Array.from(files);
+      setPhotos((prevPhotos) => [...prevPhotos, ...newPhotos]);
+    }    
+  };
+
+  const handleInputClick = () => {
+    fileInput.current?.click();
+  };
+  console.log(photos);
 
   return (
     <div className="setUp_photos">
@@ -22,8 +37,21 @@ const SetUpPhotos = () => {
         <img className="set_up_profile_photo" src={assets.userProfile} alt="" />
       </div>
       <div>
-        <p className="add_photos">Add Photos</p>
-        <div className="set_up_all_photos"></div>
+        <input
+          ref={fileInput}
+          style={{ display: "none" }}
+          type="file"
+          multiple
+          onChange={addPhotos}
+        ></input>
+        <p onClick={handleInputClick} className="add_photos">
+          Add Photos
+        </p>
+        <div className="set_up_all_photos">
+          {photos.map((photo)=> {
+            return <img src={URL.createObjectURL(photo)} alt="" />;
+          })}
+        </div>
       </div>
       <button onClick={finish}>Finish</button>
     </div>
