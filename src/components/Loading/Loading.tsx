c1d45d72b4b0b5ca8f4dd4d5a72660d8feb1b9e8
Petko Petkov <petkozlatilov1234@gmail.com>
2024-08-28 10:20:35 +0300

    chore: Add Loading component for displaying spinner during async operations

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,20 @@
+import "./Loading.scss";
+
+const Loading = () => {
+  return (
+    <div className="spinner">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Loading;
