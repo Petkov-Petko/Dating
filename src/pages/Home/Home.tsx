922b972969fa8a85ed7df3ca745076f913021499
Petko Petkov <petkozlatilov1234@gmail.com>
2024-08-30 11:49:27 +0300

    Add Home page with SideBar and Slide components

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,12 @@
+import "./Home.scss"
+import SideBar from "../../components/SideBar/SideBar"
+import Slide from "../../components/Slide/Slide"
 
 const Home = () => {
   return (
-    <div>
-      homeee
+    <div className="home_container">
+      <SideBar/>
+      <Slide/>
     </div>
   )
 }
