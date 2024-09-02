d8fed063403c342f288a2b62408111eb75e1c842
Petko Petkov <petkozlatilov1234@gmail.com>
2024-09-02 11:53:21 +0300

    Update userDetails interface in types.ts

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,22 +1,27 @@
 export interface userCredentials {
-    email: string;
-    username: string;
-    uid: string;
+  email: string;
+  username: string;
+  uid: string;
 }
 
 export interface userDetails {
-    filter(arg0: (user: string) => boolean): import("react").SetStateAction<userDetails | null | undefined>;
-    uid: string;
-    firstName: string;
-    lastName: string;
-    phoneNumber: string;
-    birthDate: string;
-    gender: string;
-    address: string;
-    country: string;
-    city: string;
-    title: string;
-    description: string;
-    profilePhoto?: string;
-    photos?: string[];
+  filter(
+    arg0: (user: string) => boolean
+  ): import("react").SetStateAction<userDetails | null | undefined>;
+  uid: string;
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
+  birthDate: string;
+  gender: string;
+  address: string;
+  country: string;
+  city: string;
+  title: string;
+  description: string;
+  profilePhoto?: string;
+  photos?: string[];
+  likes?: {
+    [key: string]: boolean;
+  };
 }
