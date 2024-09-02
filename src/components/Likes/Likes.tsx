db15d5d1826518e50a9151777edbfd553510a546
Petko Petkov <petkozlatilov1234@gmail.com>
2024-09-02 11:52:44 +0300

    Getting all the likes that the user has

--- src/components/Likes/Likes.tsx
+++ src/components/Likes/Likes.tsx
@@ -1,11 +1,29 @@
 import "./Likes.scss";
+import { getUsers } from "../../service/db-service";
+import { useSelector } from "react-redux";
+import { RootState } from "../../app/store";
+import { useState, useEffect } from "react";
+import { userDetails } from "../../types/types";
 
 const Likes = () => {
-  return (
-    <div>
-      likes
-    </div>
-  )
-}
+  const userId = useSelector((state: RootState) => state.data.user.user?.uid);
+  const [likesIds, setLikesIds] = useState<string[]>([]);
 
-export default Likes
+  useEffect(() => {
+    const fetchLikes = async () => {
+      const allUsers = Object.values(await getUsers()) as userDetails[];
+          
+      const likes = allUsers
+        .filter((user: userDetails) => user && user.likes && userId && user.likes[userId])
+        .map((user: userDetails) => user.uid);
+    
+      setLikesIds(likes);
+      console.log(likes);
+    };
+    fetchLikes();
+  }, [userId]);
+
+  return <div>likes</div>;
+};
+
+export default Likes;
