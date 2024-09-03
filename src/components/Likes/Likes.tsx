89d59af3c789bb3c8bde8bcdf512fbb4f3432c3e
Petko Petkov <petkozlatilov1234@gmail.com>
2024-09-03 10:36:38 +0300

    Added the users that liked the current user and the time from the like to the current time

--- src/components/Likes/Likes.tsx
+++ src/components/Likes/Likes.tsx
@@ -1,29 +1,53 @@
 import "./Likes.scss";
-import { getUsers } from "../../service/db-service";
+import { getUsers, getUser } from "../../service/db-service";
 import { useSelector } from "react-redux";
 import { RootState } from "../../app/store";
 import { useState, useEffect } from "react";
 import { userDetails } from "../../types/types";
+import { calculateTimeDifference } from "../../service/utils";
 
 const Likes = () => {
   const userId = useSelector((state: RootState) => state.data.user.user?.uid);
-  const [likesIds, setLikesIds] = useState<string[]>([]);
+  const [likes, setLikes] = useState<{ user: userDetails; date: string }[]>([]);
 
   useEffect(() => {
     const fetchLikes = async () => {
       const allUsers = Object.values(await getUsers()) as userDetails[];
-          
-      const likes = allUsers
-        .filter((user: userDetails) => user && user.likes && userId && user.likes[userId])
-        .map((user: userDetails) => user.uid);
-    
-      setLikesIds(likes);
+
+      const likes = await Promise.all(
+        allUsers
+          .filter(
+            (user: userDetails) =>
+              user && user.likes && userId && user.likes[userId]
+          )
+          .map(async (user: userDetails) => {
+            const userDetails = await getUser(user.uid);
+            return {
+              user: userDetails,
+              date: user.likes && userId ? user.likes[userId] : "",
+            };
+          })
+      );
+
       console.log(likes);
+      setLikes(likes);
     };
     fetchLikes();
   }, [userId]);
 
-  return <div>likes</div>;
+  return (
+    <div className="like_container">
+      {likes.map((like) => (
+        <div key={like.user.uid} className="like">
+          <img src={like.user.profilePhoto} alt="" />
+          <h4>
+            {like.user.firstName} {like.user.lastName}
+          </h4>
+          <p>Likes you {calculateTimeDifference(like.date)}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Likes;
