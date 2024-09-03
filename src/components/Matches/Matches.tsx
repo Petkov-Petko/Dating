b029de44691f86fe71e9fa80f8adabf47c791b98
Petko Petkov <petkozlatilov1234@gmail.com>
2024-09-03 17:55:03 +0300

    Added functionality that checks if the user has matches with other users and show them in the UI

--- src/components/Matches/Matches.tsx
+++ src/components/Matches/Matches.tsx
@@ -1,11 +1,49 @@
-import "./Matches.scss"
+import "./Matches.scss";
+import { useState, useEffect } from "react";
+import { getLikesIds, getUser } from "../../service/db-service";
+import { useSelector } from "react-redux";
+import { RootState } from "../../app/store";
+import { userDetails } from "../../types/types";
 
 const Matches = () => {
+  const uid = useSelector((state: RootState) => state.data.user.user?.uid);
+  const [matches, setMatches] = useState<userDetails[]>([]);
+
+  useEffect(() => {
+    const fetchMatches = async () => {
+      const userLikes = await getLikesIds(uid ?? "");
+
+      const matches = await Promise.all(
+        userLikes.map(async (like) => {
+          const userDetails = await getUser(like);
+          if (userDetails.likes && uid && userDetails.likes[uid]) {
+            return userDetails;
+          }
+          return null;
+        })
+      );
+
+      const filteredMatches = matches.filter((match) => match !== null);
+      setMatches(filteredMatches);
+    };
+
+    fetchMatches();
+  }, [uid]);
+
   return (
     <div>
-      Matches
+      <div className="like_container">
+        {matches.map((match) => (
+          <div key={match.uid} className="like">
+            <img src={match.profilePhoto} alt="" />
+            <h4>
+              {match.firstName} {match.lastName}
+            </h4>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Matches
+export default Matches;
