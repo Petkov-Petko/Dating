4f817216aa9dbe8938c794e85356f3578519b02c
Petko Petkov <petkozlatilov1234@gmail.com>
2024-09-06 20:42:07 +0300

    Added verified image

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -1,7 +1,9 @@
 import userProfile from "./user.png";
 import logo from "./logo.png";
+import verifiedLogo from "./verifiedLogo.png";
 
 export const assets = {
   userProfile,
-  logo
+  logo,
+  verifiedLogo,
 };
