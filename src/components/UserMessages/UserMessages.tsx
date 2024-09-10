66239b44a239ef1e6d08451a73d86d7c9e6bea04
Petko Petkov <petkozlatilov1234@gmail.com>
2024-09-10 16:46:25 +0300

    Refactor UserMessages component to fetch and display user chats
    Users can see there Chats (Other user name, photo and last message form the chat if there is one)
    
    When the chat is clicked the user is routed to the messages route with the chat id

--- src/components/UserMessages/UserMessages.tsx
+++ src/components/UserMessages/UserMessages.tsx
@@ -1,15 +1,66 @@
 import "./UserMessages.scss";
+import { getChats, getUser } from "../../service/db-service";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../app/store";
+import { chats } from "../../types/types";
+import { useNavigate, useParams } from "react-router-dom";
+
+interface chatDetails {
+  id: string;
+  otherUserPhoto: string;
+  otherUserName: string;
+  lastMessage : string
+}
 
 const UserMessages = () => {
+  const { id = "" } = useParams<{ id: string }>();
+  const navigate = useNavigate();
+  const uid = useSelector((state: RootState) => state.data.user.user?.uid);
+  const [chatDetails, setChatDetails] = useState<chatDetails[]>([]);
+console.log(id);
+
+  useEffect(() => {
+    const fetchChats = async () => {
+      const userChats = await getChats(uid ?? "");
+      console.log(userChats);
+
+      const chatDetailsPromises = userChats.map(async (chat: chats) => {
+        const otherUserId = chat.participants.find(
+          (participant: string) => participant !== uid
+        );
+        if (otherUserId) {
+          const otherUser = await getUser(otherUserId);
+          const lastMessage = chat.messages && chat.messages.length > 0 ? chat.messages[chat.messages.length - 1] : null;
+
+          return {
+            id: chat.id,
+            otherUserPhoto: otherUser.profilePhoto,
+            otherUserName: `${otherUser.firstName} ${otherUser.lastName}`,
+            lastMessage: lastMessage ? lastMessage.text : "No messages yet",
+          };
+        }
+      });
+
+      const chatDetails = (await Promise.all(chatDetailsPromises)).filter((chat) => chat !== undefined);
+      setChatDetails(chatDetails);
+      console.log(chatDetails);
+    };
+
+    fetchChats();
+  }, [uid]);
+
   return (
     <div className="messages_container">
       <h1>Messages</h1>
       <div className="all_messages">
-        <div className="single_message">
-          <img src="https://via.placeholder.com/50" alt="" />
-          <h4>Petko Petkov</h4>   
-          <p>Last message</p>
-        </div>
+        {chatDetails.map((chat, index) => (
+          <div onClick={()=> navigate(`/messages/${chat.id}`)} className="single_message" key={index}>
+            <img src={chat.otherUserPhoto} alt="" />
+            <h4>{chat.otherUserName}</h4>
+           <p>{chat.lastMessage}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
