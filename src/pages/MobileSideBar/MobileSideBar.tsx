3642efa86b534c13baf628395924ee343415c810
Petko Petkov <petkozlatilov1234@gmail.com>
2024-09-11 22:07:31 +0300

    Added mobile component to be shown when the screen width is low

---
+++ src/pages/MobileSideBar/MobileSideBar.tsx
@@ -0,0 +1,12 @@
+import "./MobileSideBar.scss"
+import SideBar from '../../components/SideBar/SideBar'
+
+const MobileSideBar = () => {
+  return (
+    <div className="mobileSideBar">
+      <SideBar/>
+    </div>
+  )
+}
+
+export default MobileSideBar
