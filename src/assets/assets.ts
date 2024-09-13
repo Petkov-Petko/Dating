420ff3692b2e6591bd580b1b594cde320e22d797
Petko Petkov <petkozlatilov1234@gmail.com>
2024-09-13 09:30:01 +0300

    Add landing page image to assets

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -1,9 +1,11 @@
 import userProfile from "./user.png";
 import logo from "./logo.png";
 import verifiedLogo from "./verifiedLogo.png";
+import Landing from "./landing.png";
 
 export const assets = {
   userProfile,
   logo,
   verifiedLogo,
+  Landing,
 };
