cd03d076f0958922e201ea5dc0bb1d9f8a645fbb
Petko Petkov <petkozlatilov1234@gmail.com>
2024-09-13 18:43:46 +0300

    added video and photo to assets

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -2,10 +2,12 @@ import userProfile from "./user.png";
 import logo from "./logo.png";
 import verifiedLogo from "./verifiedLogo.png";
 import Landing from "./landing.png";
+import homeVideo from "./homeVideo.mp4";
 
 export const assets = {
   userProfile,
   logo,
   verifiedLogo,
   Landing,
+  homeVideo,
 };
