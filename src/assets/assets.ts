8aa57454deb30b38284417d99cebafa43f1456f4
Petko Petkov <petkozlatilov1234@gmail.com>
2024-09-14 15:44:00 +0300

    Add FAQs data to assets.ts

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -21,3 +21,38 @@ export const assets = {
   cheers,
   fire,
 };
+
+export const faqData = [
+  {
+    question: "How do I find matches?",
+    answer: "Swipe through profiles based on your preferences and like or pass. If both you and another user like each other, it’s a match!",
+  },
+  {
+    question: "How can I send messages?",
+    answer: "Once you’ve matched with someone, you can start messaging them directly.",
+  },
+  {
+    question: "What should I do if I forget my password?",
+    answer: "Use the `Forgot Password`` option on the login page to reset it via email.",
+  },
+  {
+    question: "Can I use the app on multiple devices?",
+    answer: "Yes, you can log in to your account from multiple devices, and your information will stay synchronized.",
+  },
+  {
+    question: "Is my data secure?",
+    answer: "Yes, we use the latest security measures to protect your data and privacy.",
+  },
+  {
+    question: "How does the matching algorithm work?",
+    answer: "Our algorithm uses your preferences, profile information, and activity to suggest compatible matches.",
+  },
+  {
+    question: "How do I update my profile information?",
+    answer: "Go to your nav bar and select `Profile` to update your information and photos.",
+  },
+  {
+    question: "Can I see who viewed my profile?",
+    answer: "No but you can see who liked you.",
+  },
+];
